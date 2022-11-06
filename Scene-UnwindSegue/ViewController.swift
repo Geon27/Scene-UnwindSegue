5376e832d3ff8cb7b7213158e84de91c0059711d
Geon_27 <yukgeon98527@gmail.com>
2022-11-06 16:49:20 +0900

    1106_final

--- Scene-UnwindSegue/ViewController.swift
+++ Scene-UnwindSegue/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func unwindToVC(_segue: UIStoryboardSegue) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
